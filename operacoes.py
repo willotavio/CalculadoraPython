1eb0b7402afa0b26a6e3f9a4e4e4d83ea4de5987
willotavio <willaotavio@gmail.com>
2022-11-05 12:25:14 -0300

    Função format adicionada aos prints

--- operacoes.py
+++ operacoes.py
@@ -3,25 +3,25 @@ def soma():
     a = float(input("1º valor: "))
     b = float(input("2º valor: "))
     resultado = a + b
-    print("Resultado: %.2f + %.2f = %.2f" % (a, b, resultado))
+    print("Resultado: {:.2f} + {:.2f} = {:.2f}".format(a, b, resultado))
 
 def subtracao():
     print("Subtração")
     a = float(input("1º valor: "))
     b = float(input("2º valor: "))
     resultado = a - b
-    print("Resultado: %.2f - %.2f = %.2f" % (a, b, resultado))
-
+    print("Resultado: {:.2f} - {:.2f} = {:.2f}".format(a, b, resultado))
+  
 def multiplicacao():
     print("Multiplicação")
     a = float(input("1º valor: "))
     b = float(input("2º valor: "))
     resultado = a * b
-    print("Resultado: %.2f * %.2f = %.2f\n" % (a, b, resultado))
-
+    print("Resultado: {:.2f} * {:.2f} = {:.2f}".format(a, b, resultado))
+  
 def divisao():
     print("Divisão")
     a = float(input("1º valor: "))
     b = float(input("2º valor: "))
     resultado = a / b
-    print("Resultado: %.2f / %.2f = %.2f\n" % (a, b, resultado))
+    print("Resultado: {:.2f} / {:.2f} = {:.2f}".format(a, b, resultado))
